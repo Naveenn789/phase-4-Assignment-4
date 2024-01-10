050729570b568ace53a6250f7d0ee64a324c9e13
Naveenn789 <149990658+Naveenn789@users.noreply.github.com>
2024-01-10 15:22:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,10 +5,11 @@ using ConAppProductsJenkins;
 SortedList<int,Product> products = new SortedList<int,Product>()
 { };
 products.Add(1, new Product() { PName = "Oreo Kit Kat", PBrand = " Nestle", PPrice = 120, ManufacturingDate = new DateTime(day: 04, month: 07, year: 2023), ExpiryDate = new DateTime(day: 12, month: 09, year: 2025) });
-    products.Add(2, new Product(){PName="Wheat Atta",PBrand="Ashirawd",PPrice=273.74,ManufacturingDate=new DateTime(day:11,month:11,year:2023),ExpiryDate= new DateTime(day:23,month:02,year:2024)});
+products.Add(2, new Product(){PName="Wheat Atta",PBrand="Ashirawd",PPrice=273.74,ManufacturingDate=new DateTime(day:11,month:11,year:2023),ExpiryDate= new DateTime(day:23,month:02,year:2024)});
 products.Add(3, new Product() { PName = "FiveStar Fuse", PBrand = "Cadburry", PPrice = 75.00, ManufacturingDate = new DateTime(day: 19, month: 03, year: 2021), ExpiryDate = new DateTime(day: 02, month: 12, year: 2026) });
 products.Add(4, new Product() { PName = "Tublar Battery", PBrand = "  Amaron", PPrice = 9330.72, ManufacturingDate = new DateTime(day: 30, month: 12, year: 2023), ExpiryDate = new DateTime(day: 01, month: 12, year: 2031) });
 products.Add(5, new Product() { PName = "Hair Shampoo", PBrand = "Head&Sholder", PPrice = 220.80, ManufacturingDate = new DateTime(day: 15, month: 10, year: 2023), ExpiryDate = new DateTime(day: 31, month: 05, year: 2025) });
+products.Add(6, new Product() { PName = "Hair Oil", PBrand = "Parachute", PPrice = 200.80, ManufacturingDate = new DateTime(day: 08, month: 02, year: 2022), ExpiryDate = new DateTime(day: 10, month: 12, year: 2025) });
 
 
 Console.WriteLine("Index Value \t Product Name \t Product Brand \t Product Price \t Manufacturing Date \t Expire Date");
